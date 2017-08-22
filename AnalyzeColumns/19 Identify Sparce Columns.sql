2347b297073811e5f32f7f90d9b0899d31b8f90a
squazi <sunnaquazi@gmail.com>
2017-08-22 13:15:22 -0500

    Delete 19 Identify Sparce Columns.sql

--- AnalyzeColumns/19 Identify Sparce Columns.sql
+++
@@ -1,6 +0,0 @@
-
-select *, cast(percent_of_total as decimal(9,3)) as pct 
-from Distribution_table
-where cast(percent_of_total as decimal(9,3)) > 80 and DATAVALUES in ('NULL','.')
-order by 3 desc;
-
